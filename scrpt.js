fddaca97d7691901caa2af17b174bf45d57ef5df
Breydenmgh <omkargulave.2007@gmail.com>
2025-09-07 14:31:58 +0530

    Add files via upload

---
+++ scrpt.js
@@ -0,0 +1,15 @@
+function showFeedback() {
+    document.getElementById('feedback-modal').style.display = 'block';
+}
+
+function hideFeedback() {
+    document.getElementById('feedback-modal').style.display = 'none';
+}
+
+function showUpload() {
+    document.getElementById('upload-modal').style.display = 'block';
+}
+
+function hideUpload() {
+    document.getElementById('upload-modal').style.display = 'none';
+}
